b5efdb2248b87345387a1171c51c1bcb9d4e5d9e
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-03-21 21:52:15 +0600

    templates

--- core/views.py
+++ core/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author':'WazedK',
+        'title':'Blog Post-1',
+        'content':'Blog Post Content',
+        'date':'August 20, 2022'
+    },
+    {
+        'author':'WazedK',
+        'title':'Blog Post-2',
+        'content':'Blog Post Content',
+        'date':'August 21, 2022'
+    },
+    {
+        'author':'WazedK',
+        'title':'Blog Post-3',
+        'content':'Blog Post Content',
+        'date':'August 23, 2022'
+    },
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'home.html', context)
+    
     
 def about(request):
-    return HttpResponse('<h1>About Blog</h1>')
+    return render(request, 'about.html', {'title':'About'})
+    
