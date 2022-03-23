b78f0bdc8c681c53ad409f94d93a50cdc898fc1e
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-03-23 17:20:32 +0600

    migrations

--- core/views.py
+++ core/views.py
@@ -1,31 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author':'WazedK',
-        'title':'Blog Post-1',
-        'content':'Blog Post Content',
-        'date':'August 20, 2022'
-    },
-    {
-        'author':'WazedK',
-        'title':'Blog Post-2',
-        'content':'Blog Post Content',
-        'date':'August 21, 2022'
-    },
-    {
-        'author':'WazedK',
-        'title':'Blog Post-3',
-        'content':'Blog Post Content',
-        'date':'August 23, 2022'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'home.html', context)
     
