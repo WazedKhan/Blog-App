476e8f8d7079da59037dce52b253815da0d74998
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-03-24 20:00:22 +0600

    login system

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
+from re import template
 from django.contrib import admin
 from django.urls import path,include
 from users import views as user_views
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('core.urls')),
     path('register/', user_views.register, name='register' ),
+    path('login/', auth_view.LoginView.as_view(template_name = 'login.html'), name='login' ),
+    path('logout/', auth_view.LogoutView.as_view(), name='logout'),
 
 ]
