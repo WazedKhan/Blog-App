7051a4d17fde5305dff79fde9399d477d28bb7ac
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-04-01 22:20:48 +0600

    dynamic route to profile

--- blog/urls.py
+++ blog/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path('posts/', include('postApi.urls')),
 
 ] 
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
