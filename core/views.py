be81d9fca3371a970fd2507fafb12600fb1f6756
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-04-03 14:43:42 +0600

    clase based post list view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
+
 
 
 def home(request):
@@ -7,6 +10,16 @@ def home(request):
         'posts':Post.objects.all()
     }
     return render(request, 'home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+    
+class PostDetailView(DetailView):
+    model = Post
     
     
 def about(request):
